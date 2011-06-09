be69a2fe54cb55e8232a0359157f29d2f9fa4f77
Ryan Muller <cognitionmachine@gmail.com>
2011-06-08 20:01:24 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shuff::Application.config.secret_token = '3bf383ac7b738599b7a4f3aa8987f640cd61c51c4573cea60f3ec99b13f17fd377fdbe916968f5a9edc7170e4b818105ad1baac1ab5e86b273690f4a5115e302'
